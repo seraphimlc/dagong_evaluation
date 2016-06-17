3f0e83f4c2e87a1d6c1628a7b647f635f267a012
lc <lgd_lc@163.com>
2016-06-17 23:21:45 +0800

    alicloud

--- service/src/main/java/com/dagong/service/SearchService.java
+++ service/src/main/java/com/dagong/service/SearchService.java
@@ -2,20 +2,18 @@ package com.dagong.service;
 
 import com.alibaba.fastjson.JSON;
 import com.dagong.pojo.Evaluation;
-import com.dagong.pojo.Job;
-import com.dagong.pojo.JobEvaluation;
-import com.dagong.pojo.UserEvaluation;
 import org.elasticsearch.action.bulk.BulkRequestBuilder;
 import org.elasticsearch.action.bulk.BulkResponse;
 import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
@@ -35,11 +33,18 @@ public class SearchService {
     private final String USER_EVALUATION_TYPE = "user";
     private final String COMPANY_EVALUATION_TYPE = "company";
 
+    @Value("${searchEngine.address}")
+    private String searchAddress = null;
 
-    public SearchService() {
+    @Value("${searchEngine.port}")
+    private int searchPort = 0;
+
+
+    @PostConstruct
+    public void init() {
         try {
             transportClient = TransportClient.builder().build()
-                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName("172.16.54.144"), 9300));
+                    .addTransportAddress(new InetSocketTransportAddress(InetAddress.getByName(searchAddress), searchPort));
         } catch (UnknownHostException e) {
             e.printStackTrace();
         }
